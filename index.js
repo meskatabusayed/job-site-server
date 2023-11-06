8fb4fd4f12d6ae1646f80a5eb251194a86982407
meskatabusayed <meskatabusayed@gmail.com>
2023-11-07 03:01:04 +0600

    Implement error logging and monitoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,12 +29,9 @@ async function run() {
     await client.connect();
 
     const jobCollection = client.db('jobDB').collection('job');
+    const bidCollection = client.db('jobDB').collection('bid');
 
-    app.get('/job' , async(req , res) => {
-        const cursor = jobCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    
 
 
     app.get('/job/:category' , async(req , res) => {
@@ -46,6 +43,34 @@ async function run() {
     })
 
 
+    app.get('/jobs/:id' , async(req , res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobCollection .findOne(query);
+      res.send(result);
+  })
+
+// job post data
+  app.get('/job' , async(req , res) => {
+    let query ={};
+    if(req.query?.employerEmail){
+      query = { employerEmail: req.query.employerEmail}
+    } 
+    const result = await jobCollection.find(query).toArray();
+    res.send(result);
+    
+  })
+
+  
+
+  app.delete('/job/:id' , async(req , res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await jobCollection.deleteOne(query)
+    res.send(result);
+  })
+
+
 
 
     app.post('/job' , async(req , res) => {
@@ -53,6 +78,26 @@ async function run() {
         console.log(newJob);
         const result = await jobCollection.insertOne(newJob);
         res.send(result);
+    })
+
+
+    // Bids
+
+    app.get('/bid' , async(req , res) => {
+      
+      let query ={};
+      if(req.query?.email){
+        query = { email: req.query.email }
+      } 
+      const result = await bidCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/bid' , async(req , res) => {
+      const newBid = req.body;
+      console.log(newBid);
+      const result = await bidCollection.insertOne(newBid);
+      res.send(result);
     })
 
 
